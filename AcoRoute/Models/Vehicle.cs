04720a6d29471709d3f3bbc77e0154ac32e452de
churko <kuschnire@gmail.com>
2017-10-10 16:53:44 -0300

    Added Vehicle Class

---
+++ AcoRoute/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AcoRoute.Models
+{
+    public class Vehicle
+    {
+        [Key]
+        public int VehicleId { get; set; }
+        public string Plate { get; set; }
+        public string VehicleType { get; set; }
+        public string Make { get; set; }
+        public int Model { get; set; }
+        public int Capacity { get; set; }
+        public DateTime LastServiceDate { get; set; }
+    }
+}
