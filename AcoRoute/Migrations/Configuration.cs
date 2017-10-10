e1797daf47a34834b32d25041556d12447f3ebd4
churko <kuschnire@gmail.com>
2017-10-10 18:04:21 -0300

    Added automatic migrations

---
+++ AcoRoute/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace AcoRoute.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<AcoRoute.Models.AcoRouteContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "AcoRoute.Models.AcoRouteContext";
+        }
+
+        protected override void Seed(AcoRoute.Models.AcoRouteContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
