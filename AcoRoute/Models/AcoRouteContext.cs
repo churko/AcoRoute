a31fffb7622d6ffb342b222cef3ec7c73be40260
churko <kuschnire@gmail.com>
2017-10-10 17:07:39 -0300

    Add Controller and Views for Vehicle model. Added string connection for aplication data management

---
+++ AcoRoute/Models/AcoRouteContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AcoRoute.Models
+{
+    public class AcoRouteContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public AcoRouteContext() : base("name=AcoRouteContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<AcoRoute.Models.Vehicle> Vehicles { get; set; }
+    }
+}
