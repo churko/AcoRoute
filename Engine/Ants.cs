5004a11dca8e985b0720438f7c5320f2a7b0d0b2
churko <kuschnire@gmail.com>
2017-12-07 17:26:09 -0300

    Created Ants class

--- Engine/Ants.cs
+++ Engine/Ants.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Engine
 {
     public class Ants
     {
+        public int RouteLength { get; set; }
+
+        public List<int> Route { get; set; }
+
+        public List<int> Visited { get; set; }
     }
 }
