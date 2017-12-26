2d6980ff7a93a4e354d6f827fbda16d9338db7c8
churko <kuschnire@gmail.com>
2017-12-26 19:02:20 -0300

    Revert "Created Ants class"
    This reverts commit 5004a11dca8e985b0720438f7c5320f2a7b0d0b2.

--- Engine/Ants.cs
+++ Engine/Ants.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Engine
 {
     public class Ants
     {
-        public int RouteLength { get; set; }
-
-        public List<int> Route { get; set; }
-
-        public List<int> Visited { get; set; }
     }
 }
