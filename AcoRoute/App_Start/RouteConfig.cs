3a7144b96914c5509dea56926f3552b757f568de
churko <kuschnire@gmail.com>
2018-01-16 21:54:17 -0300

    Added Icons

--- AcoRoute/App_Start/RouteConfig.cs
+++ AcoRoute/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AcoRoute
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Persons", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
